10216903e78be66b27e4dc8f2a3a0d212385e27b
Rachel Ai <65563426+raiccoon@users.noreply.github.com>
2022-08-26 17:55:17 -0400

    Add files via upload - data transformation
    Edited versions of Allen's transformation code. I swapped out the list of column names and used the get_columns method to generate them instead, otherwise the code is essentially the same

---
+++ transform_log.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+from get_columns import get_columns
+
+# raw_data: Raw data extracted from XML file, with empty rows removed
+df = pd.read_excel('data\\raw_data.xlsx')
+print(df)
+
+# Generate column names
+metrics = ["Time in Notes per Day", "Time in Notes per Appointment", "Progress Note Length",
+    "Length of Documentation per Appointment", "Note Composition Method by Author - Manual"]
+
+columns = get_columns(metrics)
+
+# Perform log transformation, output to new columns with "_log" tag
+# Can be changed by using np.log2.
+for s in columns:
+    df[s + '_log'] = np.log10(df[s])
+print(df)
+
+df.to_excel('data\\log_transformed_data.xlsx')
+
