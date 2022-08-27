b5fb7fe7b077fe74806b9a5224e182511597598a
Rachel Ai <raiccoon936@gmail.com>
2022-08-27 02:18:26 -0400

    Simplified get_columns

--- GetColumns.py
+++ GetColumns.py
@@ -5,13 +5,11 @@ def get_columns(metrics, suffix = ""):
     metrics (list of str): List of metrics as they would appear in EPIC Signal
     suffic (str): Optional suffix (ex. "_log" if data has been transformed)
     """
-    parse = lambda x: x.replace("-", "_").replace("'", " ").replace(" ", "_")
-
     # Create 3 variables (numerator, denominator, value) per metric , compile into column names
     columns =  []
     for m in metrics:
-        columns.append(parse(m) + "_numerator" + suffix)
-        columns.append(parse(m) + "_denominator" + suffix)
-        columns.append(parse(m) + "_value" + suffix)
+        columns.append(m.replace(" ", "_") + "_numerator" + suffix)
+        columns.append(m.replace(" ", "_") + "_denominator" + suffix)
+        columns.append(m.replace(" ", "_") + "_value" + suffix)
 
     return columns
